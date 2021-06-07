1d09e3f5a957797f4ddff5d32117663fa364b149
Joel Webb <webbj98@gmail.com>
2021-06-07 09:56:37 -0500

    added ability to create and edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,36 @@ class ArticlesController < ApplicationController
 	  @articles = Article.all
 
 	end
+
+	def new
+	  # we need to initilize article here so that he html will work 
+	  # in the case of producing errors
+	  @article = Article.new
+	end
+
+	def edit
+	  @article = Article.find(params[:id])
+	end
+
+	def create
+	  @article = Article.new(
+		  params.require(:article).permit(:title, :description))
+	  if @article.save
+		flash[:notice] = "Articel was created successfully."
+		redirect_to @article
+	  else
+		render 'new'
+	  end
+	end
+
+	def update
+	  
+	  @article = Article.find(params[:id])
+	  if @article.update(params.require(:article).permit(:title, :description))
+		flash[:notice] = "Article was updated successfully."
+		redirect_to @article
+	  else
+		render 'edit'
+	  end
+	end
 end
