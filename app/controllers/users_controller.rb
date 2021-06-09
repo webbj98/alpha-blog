e394200e88834c873d8fa0407dc4eaa721702c61
Joel Webb <webbj98@gmail.com>
2021-06-09 16:30:54 -0500

    added article listing for user and user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
