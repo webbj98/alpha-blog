3c66abc170b39f8dda4b7076cda77269f2bcbd9b
Joel Webb <webbj98@gmail.com>
2021-06-14 15:16:26 -0500

    added admin access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account all associated articles deleted"
     redirect_to articles_path
   end
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
 
   private
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit or delete your own profile"
       redirect_to @user
     end
