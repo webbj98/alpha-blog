bfa0b18dc54af822e530dca0411c14ed52b9d772
Joel Webb <webbj98@gmail.com>
2021-06-14 14:31:30 -0500

    added controller restrictions and ability to delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_article, only:[:show, :edit, :update]
+  before_action :set_article, only:[:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def new 
     @user = User.new
@@ -43,6 +45,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account all associated articles deleted"
+    redirect_to articles_path
+  end
   private 
 
   def set_article
@@ -51,6 +59,14 @@ class UsersController < ApplicationController
   def user_params
 
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  private
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own profile"
+      redirect_to @user
+    end
   end
 
 end
