cbd1432b69c436f4b86d39cbcea4a6fffa238f53
Joel Webb <webbj98@gmail.com>
2021-06-16 09:50:46 -0500

    added integration tests and functionality for listing and creating categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,40 @@
 class CategoriesController < ApplicationController
+
+
+  before_action :set_category, only:[:show]
+
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+        
+        
+    end
   end
 
   def show
   end 
 
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 2)
   end
+
+  private
+ 
+  def set_category
+	@category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  
 end
